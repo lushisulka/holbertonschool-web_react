3f19c7995639951b4e823b6a5a71eb8e1326c752
lushisulka <sulkaengjellushe5@gmail.com>
2025-09-10 17:21:22 +0200

    check task 0

---
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import holbertonLogo from './assets/holberton-logo.jpg'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="App-header">
+        <img src={holbertonLogo} alt='holberton logo'/>
+        <h1 style={{ color: '#e1003c'}} >School dashboard</h1>
+    </div>
+    <div className='App-body'>
+        <p>Login to access the full dashboard</p>
+    </div>
+    <div className='App-footer'>
+        <p>Copyright the current year e.g: 2025 - holberton School</p>
+    </div>
+    </>
+      
+  )
+}
+
+export default App
