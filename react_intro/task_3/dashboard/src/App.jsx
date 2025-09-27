2f404c2eb78ef770b621ed06625bcd05b8388eec
lushisulka <sulkaengjellushe5@gmail.com>
2025-09-27 16:55:44 +0200

    check tast 3

--- react_intro/task_3/dashboard/src/App.jsx
+++ react_intro/task_3/dashboard/src/App.jsx
@@ -17,13 +17,13 @@ function App() {
 
       <div className='App-body'>
         <p> Login to access the full dashboard</p>
-        <label htmlFor="email">email</label>
+        <label htmlFor="email">Email</label>
         <input type="email" id="email"></input>
 
-        <label htmlFor="password">password</label>
+        <label htmlFor="password">Password</label>
         <input type="password" id="password"></input>
 
-        <button type="submit">Ok</button>
+        <button type="submit">OK</button>
       </div>
       <div className='App-footer'>
         <p>Copyright {getCurrentYear()} -  {getFooterCopy(true)}</p>
