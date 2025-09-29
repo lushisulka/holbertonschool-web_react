8446f8bde7aba6354bc62989eee6ccb57f2de550
lushisulka <sulkaengjellushe5@gmail.com>
2025-09-29 16:36:29 +0200

    changes

--- react_props/task_0/dashboard/src/App/App.spec.js
+++ react_props/task_0/dashboard/src/App/App.spec.js
@@ -1,12 +1,12 @@
-import { shallow } from 'enzyme';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-
-describe('Testing <App /> component', () => {
-    describe('App renders without crashing', () => {
-        it ('should render App withour crashing', () => {
-            const wrapper = shallow(<App />);
-            expect(wrapper.exists()).toEqual(true);
-        });
+describe("Testing <App /> component", () => {
+    it("renders App without crashing", () => {
+        render(<App />);
+        expect(screen.getByText(/School dashboard/i)).toBeInTheDocument();
+        expect(screen.getByText(/Login to access the full dashboard/i)).toBeInTheDocument();
+        expect(screen.getByText(/Holberton School/i)).toBeInTheDocument();
     });
-})
+});
